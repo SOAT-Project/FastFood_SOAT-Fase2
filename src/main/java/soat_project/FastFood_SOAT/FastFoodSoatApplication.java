499d5caa01c8ea3bb0feb2492b3d1ba9eeb1f801
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-05-01 23:02:14 -0300

    feat(init) : initial commit and basic setup

---
+++ src/main/java/soat_project/FastFood_SOAT/FastFoodSoatApplication.java
@@ -0,0 +1,13 @@
+package soat_project.FastFood_SOAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodSoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodSoatApplication.class, args);
+	}
+
+}
