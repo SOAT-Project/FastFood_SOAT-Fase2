7d5600cce6017ca82400594d5f4909a9ad445013
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-05-12 00:02:07 -0300

    feat(Product) : create usecase and execute method

---
+++ src/main/java/soat/project/fastfoodsoat/application/usecase/product/create/CreateProductUseCase.java
@@ -0,0 +1,6 @@
+package soat.project.fastfoodsoat.application.usecase.product.create;
+
+import soat.project.fastfoodsoat.application.UseCase;
+
+public abstract class CreateProductUseCase extends UseCase<CreateProductCommand, CreateProductOutput> {
+}
