21d90941f67688c1da8a51fd6598d56ab4cb78fa
Kayky Freitas <kayky_defreitas@hotmail.com>
2025-05-12 01:45:58 -0300

    docs(staff): ajustando documentacao swagger da autenticacao do staff

--- src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/AuthStaffAPI.java
+++ src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/AuthStaffAPI.java
@@ -1,6 +1,8 @@
 package soat.project.fastfoodsoat.adapter.inbound.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.AuthStaffRequest;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.AuthStaffResponse;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.DefaultApiError;
 
 @Tag(name = "Auth")
 @RequestMapping("/auth")
@@ -26,9 +29,26 @@ public interface AuthStaffAPI {
     )
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Token generated successfully"),
-                    @ApiResponse(responseCode = "404", description = "Staff was not found"),
-                    @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Token generated successfully",
+                            content = @Content(schema = @Schema(implementation = AuthStaffResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Staff was not found",
+                            content = @Content(schema = @Schema(implementation = DefaultApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "A validation error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultApiError.class))
+                    ),
             }
     )
     ResponseEntity<AuthStaffResponse> authenticate(@RequestBody AuthStaffRequest authRequest);
