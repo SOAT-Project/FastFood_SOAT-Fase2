1636f2bb1bc71a3540e30afb3c454e2fbc798c57
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-05-12 01:21:44 -0300

    feat(product) : find by id

---
+++ src/main/java/soat/project/fastfoodsoat/application/usecase/product/retrieve/get/GetProductUseCase.java
@@ -0,0 +1,6 @@
+package soat.project.fastfoodsoat.application.usecase.product.retrieve.get;
+
+import soat.project.fastfoodsoat.application.UseCase;
+
+public abstract class GetProductUseCase extends UseCase<Integer, GetProductOutput> {
+}
