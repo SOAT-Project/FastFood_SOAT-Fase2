65f24efec7108ab273d7da4ccfa1da409e0151d3
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-05-11 22:37:37 -0300

    fix(Product) : add pagination

--- src/main/java/soat/project/fastfoodsoat/domain/product/ProductGateway.java
+++ src/main/java/soat/project/fastfoodsoat/domain/product/ProductGateway.java
@@ -1,8 +1,9 @@
 package soat.project.fastfoodsoat.domain.product;
 
+import soat.project.fastfoodsoat.domain.pagination.Pagination;
+import soat.project.fastfoodsoat.domain.pagination.SearchQuery;
 import soat.project.fastfoodsoat.domain.product.productCategory.ProductCategoryId;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ProductGateway {
@@ -10,5 +11,6 @@ public interface ProductGateway {
     Product update(Product product);
     void deleteById(ProductId productId);
     Optional<Product> findById(ProductId productId);
-    List<Product> findProductByCategory(ProductCategoryId productCategoryId);
+    Pagination<Product> findProductByCategory(ProductCategoryId productCategoryId, SearchQuery searchQuery);
+    Pagination<Product> findAll(SearchQuery searchQuery);
 }
