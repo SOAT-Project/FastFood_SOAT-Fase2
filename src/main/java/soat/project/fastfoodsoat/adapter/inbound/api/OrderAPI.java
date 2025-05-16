c296207f829167e0f7720951b6961573a26e27d0
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2025-05-16 03:32:57 -0300

    feat(order): Controller e interface

---
+++ src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/OrderAPI.java
@@ -0,0 +1,49 @@
+package soat.project.fastfoodsoat.adapter.inbound.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.DefaultApiError;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.request.CreateOrderRequest;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.response.CreateOrderResponse;
+
+@Tag(name = "Order")
+@RequestMapping("/orders")
+public interface OrderAPI {
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(
+            summary = "Create a new order"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Order generated successfully",
+                            content = @Content(schema = @Schema(implementation = CreateOrderResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "A validation error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "An internal server error was thrown",
+                            content = @Content(schema = @Schema(implementation = DefaultApiError.class))
+                    )
+        }
+    )
+    ResponseEntity<CreateOrderResponse> create(@RequestBody CreateOrderRequest orderRequest);
+}
---
+++ src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/controller/OrderController.java
@@ -0,0 +1,36 @@
+package soat.project.fastfoodsoat.adapter.inbound.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import soat.project.fastfoodsoat.adapter.inbound.api.OrderAPI;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.request.CreateOrderRequest;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.response.CreateOrderResponse;
+import soat.project.fastfoodsoat.adapter.inbound.api.presenter.OrderPresenter;
+import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderCommand;
+import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderProductCommand;
+import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderUseCase;
+
+@RestController
+public class OrderController implements OrderAPI {
+
+    private final CreateOrderUseCase createOrderUseCase;
+
+    public OrderController(final CreateOrderUseCase createOrderUseCase) {
+        this.createOrderUseCase = createOrderUseCase;
+    }
+
+    @Override
+    public ResponseEntity<CreateOrderResponse> create(final CreateOrderRequest createOrderRequest) {
+        final var command = new CreateOrderCommand(
+                createOrderRequest.clientId(),
+                createOrderRequest.orderProducts()
+                        .stream()
+                        .map(CreateOrderProductCommand::from)
+                        .toList()
+        );
+
+        final var output = this.createOrderUseCase.execute(command);
+
+        return ResponseEntity.ok(OrderPresenter.present(output));
+    }
+}
