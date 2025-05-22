f10c41b203ecedf1d0700414e631ac1365ea3f07
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2025-05-22 01:13:41 -0300

    feat(payment): integração qrCode mercado pago

--- src/main/java/soat/project/fastfoodsoat/infrastructure/config/MercadoPagoConfig.java
+++
@@ -1,9 +0,0 @@
-package soat.project.fastfoodsoat.infrastructure.config;
-
-public class MercadoPagoConfig {
-
-    public static final String ACCESS = "TEST-3f0b1c2a-4d8e-4f5b-9a6c-7d0e2f3f4b5c";
-
-    public static final String URL = "https://api.mercadopago.com/v1/payments";
-
-}
