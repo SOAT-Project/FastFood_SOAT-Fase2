c198e312dca332373789fc79c2b0e8da371b22db
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-05-25 22:05:21 -0300

    feat(Order) : create update status endpoint

--- src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/OrderAPI.java
+++ src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/OrderAPI.java
@@ -8,12 +8,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.DefaultApiError;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.request.CreateOrderRequest;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.request.UpdateOrderStatusRequest;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.response.CreateOrderResponse;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.response.UpdateOrderStatusResponse;
 
 @Tag(name = "Order")
 @RequestMapping("/orders")
@@ -51,4 +51,21 @@ public interface OrderAPI {
         }
     )
     ResponseEntity<CreateOrderResponse> create(@RequestBody CreateOrderRequest orderRequest);
+
+    @PutMapping(value = "/{publicId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Update order status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Order status updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Order not found"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
+    ResponseEntity<UpdateOrderStatusResponse> updateStatus(
+            @PathVariable String publicId,
+            @RequestBody UpdateOrderStatusRequest request
+    );
+
+
+
+
 }
--- src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/controller/OrderController.java
+++ src/main/java/soat/project/fastfoodsoat/adapter/inbound/api/controller/OrderController.java
@@ -5,20 +5,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import soat.project.fastfoodsoat.adapter.inbound.api.OrderAPI;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.request.CreateOrderRequest;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.request.UpdateOrderStatusRequest;
 import soat.project.fastfoodsoat.adapter.inbound.api.model.response.CreateOrderResponse;
+import soat.project.fastfoodsoat.adapter.inbound.api.model.response.UpdateOrderStatusResponse;
 import soat.project.fastfoodsoat.adapter.inbound.api.presenter.OrderPresenter;
 import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderCommand;
 import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderOutput;
 import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderProductCommand;
 import soat.project.fastfoodsoat.application.usecase.order.create.CreateOrderUseCase;
+import soat.project.fastfoodsoat.application.usecase.order.update.changeStatus.UpdateOrderStatusCommand;
+import soat.project.fastfoodsoat.application.usecase.order.update.changeStatus.UpdateOrderStatusUseCase;
+
+import java.util.UUID;
 
 @RestController
 public class OrderController implements OrderAPI {
 
     private final CreateOrderUseCase createOrderUseCase;
+    private final UpdateOrderStatusUseCase updateOrderStatusUseCase;
 
-    public OrderController(final CreateOrderUseCase createOrderUseCase) {
+    public OrderController(final CreateOrderUseCase createOrderUseCase,
+                           final UpdateOrderStatusUseCase updateOrderStatusUseCase) {
         this.createOrderUseCase = createOrderUseCase;
+        this.updateOrderStatusUseCase = updateOrderStatusUseCase;
     }
 
     @Override
@@ -34,4 +43,13 @@ public class OrderController implements OrderAPI {
         final CreateOrderOutput output = this.createOrderUseCase.execute(command);
         return ResponseEntity.status(HttpStatus.CREATED).body(OrderPresenter.present(output));
     }
+
+    @Override
+    public ResponseEntity<UpdateOrderStatusResponse> updateStatus(final String publicId, final UpdateOrderStatusRequest request) {
+        var command = new UpdateOrderStatusCommand(UUID.fromString(publicId), request.status());
+        var output = updateOrderStatusUseCase.execute(command);
+        return ResponseEntity.ok(new UpdateOrderStatusResponse(output.orderPublicId().toString(), output.newStatus()));
+    }
+
+
 }
