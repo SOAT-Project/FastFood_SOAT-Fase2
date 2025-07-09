5eae0e8ffff54b8e77dd26ebc0be5be68a17fc88
Matheus de Sá <matheus.de.sa@outlook.com.br>
2025-07-09 00:23:14 -0300

    Atualizando a response

--- src/main/java/soat/project/fastfoodsoat/infrastructure/web/model/response/payment/MercadoPagoWebhookResponse.java
+++ src/main/java/soat/project/fastfoodsoat/infrastructure/web/model/response/payment/MercadoPagoWebhookResponse.java
@@ -2,9 +2,9 @@ package soat.project.fastfoodsoat.infrastructure.web.model.response.payment;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record UpdatePaymentToPaidStatusResponse(
-        @JsonProperty("value") String value,
-        @JsonProperty("status") String paymentStatus,
+public record MercadoPagoWebhookResponse(
+        @JsonProperty("order_public_id") String orderPublicId,
+        @JsonProperty("payment_status") String paymentStatus,
         @JsonProperty("external_reference") String externalReference
 ) {
 }
